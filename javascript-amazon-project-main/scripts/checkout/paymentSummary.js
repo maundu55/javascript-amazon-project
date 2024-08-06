f6774ed953f39e1e6d849509f1453d094db14ce1
maundu55 <marymaundu.mm@gmail.com>
2024-08-06 12:27:08 +0300

    calculating total shipping cost using JS

--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -1,10 +1,18 @@
 import {cart} from '../../data/cart';
 import { getProduct } from '../../data/products';
+import {getDeliveryOption} from '../../data/deliveryOptions.js';
+
 
 export function renderPaymentSummary(){
     let productPriceCents = 0;
+    let ShippingPriceCents = 0;
+
     cart.forEach((cartItem) => {
        const product = getProduct(cartItem.productId);
-       productPriceCents = product.priceCents * cartItem.quantity
+       productPriceCents += product.priceCents * cartItem.quantity;
+
+       const deliveryOption = getDeliveryOption(cartItem.getDeliveryOptionId);
+        ShippingPriceCents += deliveryOption.priceCents
+
     });
 }
